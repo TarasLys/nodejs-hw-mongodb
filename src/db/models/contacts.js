d9116fbfaeb89842e183c54981f26648f4196101
TarasLys <opros0609@gmail.com>
2024-10-20 00:27:06 +0300

    Ваше сообщение коммита

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,8 +1,9 @@
-import { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
+const { Schema } = mongoose;
 
 export const ContactsCollection = new Schema(
-{
+  {
     name: {
       type: String,
       required: true,
@@ -30,4 +31,38 @@ export const ContactsCollection = new Schema(
     versionKey: false,
   },
 );
+
+
+// import { Schema } from 'mongoose';
+
+
+// export const ContactsCollection = new Schema(
+// {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     phoneNumber: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//     },
+//     isFavourite: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     contactType: {
+//       type: String,
+//       enum: ['work', 'home', 'personal'],
+//       required: true,
+//       default: 'personal',
+//     },
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   },
+// );
 

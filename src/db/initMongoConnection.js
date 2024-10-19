7801b69752eb5238664d5b82181fa5da7fe92946
TarasLys <opros0609@gmail.com>
2024-10-20 00:49:57 +0300

    Ваше сообщение коммита

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,16 +10,36 @@ export const initMongoConnection = async () => {
 
     const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
 
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(connectionString);
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.error('Error while setting up mongo connection', e);
     throw e;
   }
 };
+
+// import mongoose from 'mongoose';
+// import { env } from '../utils/env.js';
+
+// export const initMongoConnection = async () => {
+//   try {
+//     const user = env('MONGODB_USER');
+//     const pwd = env('MONGODB_PASSWORD');
+//     const url = env('MONGODB_URL');
+//     const db = env('MONGODB_DB');
+
+//     const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+
+//     await mongoose.connect(connectionString, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Mongo connection successfully established!');
+//   } catch (e) {
+//     console.error('Error while setting up mongo connection', e);
+//     throw e;
+//   }
+// };
 
 
 // import mongoose from 'mongoose';

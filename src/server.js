c2215b4c4e585c039c8211a246320d3a768d3204
TarasLys <opros0609@gmail.com>
2024-10-20 00:13:47 +0300

    Ваше сообщение коммита

--- src/server.js
+++ src/server.js
@@ -29,18 +29,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
 
 	app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
@@ -67,6 +56,20 @@ export const setupServer = () => {
     res.status(200).json({
       message: "Successfully found contact with id {contactId}!",
       data: contacts,
+    });
+  });
+
+
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
     });
   });
 

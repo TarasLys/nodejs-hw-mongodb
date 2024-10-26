19cd8058f615bfe780565a0211c6c7949b642bdf
TarasLys <opros0609@gmail.com>
2024-10-26 15:19:28 +0300

    homework 3

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -24,39 +24,3 @@ export default router;
 
 
 
-//import { Router } from "express";
-// import { getAllContacts, getContactsById } from './services/contacts.js';
-
-// const router = Router();
-
-// 	router.get('/contacts', async (req, res) => {
-//     const contacts = await getAllContacts();
-
-//     res.status(200).json({
-//       status: 200,
-//       message: "Successfully found contacts!",
-//       data: contacts,
-//     });
-//   });
-
-//   router.get('/contacts/:contactsId', async (req, res, next) => {
-//     const { contactsId } = req.params;
-//     const contacts = await getContactsById(contactsId);
-
-
-// 	if (!contacts) {
-// 	  res.status(404).json({
-// 		  message: 'Contacts not found'
-// 	  });
-// 	  return;
-// 	}
-
-
-//     res.status(200).json({
-//       status: 200,
-//       message: "Successfully found contact with id {contactId}!",
-//       data: contacts,
-//     });
-//   });
-
-// export default router;

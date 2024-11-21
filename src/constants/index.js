cadefd8df7367910a4b4b015095bce22a36a9879
TarasLys <opros0609@gmail.com>
2024-11-21 20:51:34 +0200

    tree

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,3 +26,6 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
 
 };
+
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -33,6 +34,9 @@ export const setupServer = () => {
     });
   });
 
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
   app.use('*', notFoundHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
